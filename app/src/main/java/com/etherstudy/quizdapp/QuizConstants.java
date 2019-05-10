7cf919a88e398769767a03786536469affe88b6a
kjwook <cmdhema@gmail.com>
2019-05-11 08:47:02 +0900

    update server URL

--- app/src/main/java/com/etherstudy/quizdapp/QuizConstants.java
+++ app/src/main/java/com/etherstudy/quizdapp/QuizConstants.java
@@ -5,7 +5,7 @@ import java.math.BigInteger;
 public class QuizConstants {
 
 //    public static final String SERVER_IP = "http://101.101.161.251:8001";
-    public static final String SERVER_IP = "http://192.168.0.89:8001";
+    public static final String SERVER_IP = "http://52.43.10.165:8001";
     public static final String ETH_NODE_URL = "https://ropsten.infura.io/v3/8f79bace6d6440e3a40300868915d9ec";
 
     public static final String QUIZ_CONTRACT_ADDRESS = "0xf9da1791376e03da8cf6636aa65cfb72dc77169b";
